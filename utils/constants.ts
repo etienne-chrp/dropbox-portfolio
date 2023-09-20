9446d35ce29d09c34759d26d6b48cd472423dbc5
etienne-chrp <etienne-chrp@users.noreply.github.com>
2023-09-20 16:28:29 +0200

    Change WORK_MAIN_IMG_NAME to comply with number format

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
     static readonly WORK_FOLDER_PATH = '/work';
     static readonly WORK_IMG_FOLDER_NAME = 'img';
-    static readonly WORK_MAIN_IMG_NAME = 'main.jpg';
+    static readonly WORK_MAIN_IMG_NAME = '000 main.jpg';
     static readonly WORK_DESCRIPTION_NAME = 'description.md';
     static readonly WORK_INFO_NAME = 'info.md';
     static readonly ABOUT_FOLDER_PATH = '/about';
